b188424d07e5125e6a76e96f500b36cb58b026c1
batzor <32958247+batzor@users.noreply.github.com>
2022-02-09 02:16:41 +0900

    Do some cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use std::fs::File;
 use std::io::prelude::*;
 
 use reqwest::header::{HeaderName, HeaderValue};
-use reqwest::{Client, Error, RequestBuilder};
+use reqwest::{Client, Error};
 use serde_json::{Map, Value};
 
 type SubmissionEntry = Map<String, Value>;
 
+/* Login and inspect and load submissions page.
+ * On Network tab, you will see request with name ?offset=0&limit=20&lastkey=
+ * From that request copy the appropriate fields and paste it here.
+ */
 const HEADER: &str = r#"authority: leetcode.com
 sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"
 accept: */*
-x-newrelic-id: UAQDVFVRGwEAXVlbBAg=
+x-newrelic-id: {copy value and paste here}
 x-requested-with: XMLHttpRequest
 sec-ch-ua-mobile: ?0
 user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36
@@ -20,7 +24,7 @@ sec-fetch-mode: cors
 sec-fetch-dest: empty
 referer: https://leetcode.com/submissions/
 accept-language: en-US,en;q=0.9,ko-KR;q=0.8,ko;q=0.7
-cookie: NEW_PROBLEMLIST_PAGE=1; {FILL THE REST}
+cookie: {copy value and paste here}
 dnt: 1
 sec-gpc: 1"#;
 
@@ -29,22 +33,6 @@ fn filter_submissions(val: &Value) -> Option<SubmissionEntry> {
         Value::Object(x) => Some(x.clone()),
         _ => None,
     }
-}
-
-async fn get_submissions(req_builder: RequestBuilder) -> Result<Vec<SubmissionEntry>, Error> {
-    let json_obj = req_builder
-        .send()
-        .await?
-        .json::<Map<String, Value>>()
-        .await?;
-    let submissions = match json_obj["submissions_dump"].clone() {
-        Value::Array(v) => v.iter().filter_map(|val| filter_submissions(val)).collect(),
-        _ => {
-            vec![]
-        }
-    };
-
-    Ok(submissions)
 }
 
 async fn scrape_page(client: &Client, page_id: i32) -> Result<Vec<SubmissionEntry>, Error> {
@@ -61,16 +49,29 @@ async fn scrape_page(client: &Client, page_id: i32) -> Result<Vec<SubmissionEntr
             HeaderValue::from_static(header[1]),
         );
     }
-    let submissions: Result<Vec<SubmissionEntry>, Error> = get_submissions(req_builder).await;
-    submissions
+    let json_obj = req_builder
+        .send()
+        .await?
+        .json::<Map<String, Value>>()
+        .await?;
+    let submissions = match json_obj["submissions_dump"].clone() {
+        Value::Array(v) => v.iter().filter_map(|val| filter_submissions(val)).collect(),
+        _ => {
+            vec![]
+        }
+    };
+
+    Ok(submissions)
 }
 
 fn main() -> std::io::Result<()> {
+    println!("Please make sure you filled out the HEADER const with your cookie");
+
     let rt = tokio::runtime::Runtime::new().unwrap();
     let client = Client::new();
 
-    let mut page_id: i32 = 15;
-    while page_id < 23 {
+    // TODO: set maximum page_id dynamically
+    for page_id in 0..22 {
         println!("Scraping page {}...", page_id + 1);
         let submissions: Vec<SubmissionEntry> = rt.block_on(scrape_page(&client, page_id)).unwrap();
         for s in &submissions {
@@ -99,7 +100,7 @@ fn main() -> std::io::Result<()> {
             println!("Saving new solution at {}", file_path);
             file.write_all(code.as_bytes())?;
         }
-        page_id = page_id + 1;
+        // sleep a bit to prevent getting restricted
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
     return Ok(());
